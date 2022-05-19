1f2facb9798c6ee39525d916ef21129582831324
Leonard <70354786+wuleo870@users.noreply.github.com>
2022-05-19 13:13:25 -0400

    Update GameObject.java

--- logic/GameObject.java
+++ logic/GameObject.java
@@ -1,4 +1,47 @@
+public abstract class GameObject {
 
-public class GameObject {
+ 
 
-}
+       //Sprite Fields:
+
+       private int width; //Sprite Hitbox
+
+       private int height; //Sprite Hitbox
+
+       private int x; //Sprite Coords
+
+       private int y; //Sprite Coords
+
+      
+
+       //Characteristic Fields:
+
+       private int speed;
+
+      
+
+      
+
+      
+
+       //Constructor:
+
+       public GameObject(int width,int height, int x, int y, int speed) {
+
+              this.width = width;
+
+              this.height = height;
+
+              this.x = x;
+
+              this.y = y;
+
+              this.speed = speed;
+
+       }
+
+      
+
+       //Methods:
+
+      
