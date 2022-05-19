14089d26d872e89f705ab75d5e4741f533f84361
FriedRice53 <97309425+FriedRice53@users.noreply.github.com>
2022-05-19 19:49:41 -0400

    Update GraphicsMain.java

--- graphics/GraphicsMain.java
+++ graphics/GraphicsMain.java
@@ -1,1 +1,21 @@
-lol
+import processing.core.PApplet;
+
+public class GraphicsMain extends PApplet {
+
+  public static void main(String[] args) {
+    PApplet.main("GraphicsMain");
+  }
+
+  public void settings() {
+    size(1400,700);
+  }
+
+  public void setup() {
+  
+  }
+
+  public void draw() {
+    
+  }
+
+}
