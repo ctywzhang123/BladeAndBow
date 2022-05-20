71d48e44691666a09428d4ea0fd1197049b8e6f5
Leonard <70354786+wuleo870@users.noreply.github.com>
2022-05-20 12:51:51 -0400

    Update Character.java

--- logic/Character.java
+++ logic/Character.java
@@ -2,57 +2,50 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Character extends GameObject {
-                PImage sprite;
-                public Character(int width, int height, int x, int y, int speed) {
-                                super(width, height, x, y, speed);
-                }
- 
-                //Interface Methods:
+	PImage sprite;
 
-                @Override
-                public void move(int xMove, int yMove) {
-                                setX(getX() + xMove);
-                                setY(getY() + yMove);
-                }
+	public Character(int width, int height, int x, int y, int speed) {
+		super(width, height, x, y, speed);
+	}
 
- 
+	// Interface Methods:
 
-                @Override
-                public boolean collide(GameObject g) {
-                                //Check if the Weapon hitbox collides with a player hitbox
-                 
-                                //X and Y coordinates are the center of the image/hitbox rectangle
+	@Override
+	public void move(int xMove, int yMove) {
+		setX(getX() + xMove);
+		setY(getY() + yMove);
+	}
 
-                                Weapon w = new Weapon(0, 0, 0, 0, 0);
+	@Override
+	public boolean collide(GameObject g) {
+		// Check if the Weapon hitbox collides with a player hitbox
 
-                                if(g.getClass() != w.getClass()) //Ensure Weapon hits a player
-                                                return false;
-                                else {
-                                                if((this.getX() + this.getWidth()/2 >= g.getX() - g.getWidth()/2) &&
-                                                                                (this.getY() + this.getHeight()/2 >= g.getY() - g.getHeight()/2) &&
-                                                                                (this.getY() - this.getHeight()/2 <= g.getY() + g.getHeight())) {
-                                                                return true;
-                                                }
+		// X and Y coordinates are the center of the image/hitbox rectangle
 
-                                                else {
-                                                                return false;
-                                                }
+		Weapon w = new Weapon(0, 0, 0, 0, 0);
 
-                                }
+		if (g.getClass() != w.getClass()) // Ensure Weapon hits a player
+			return false;
+		else {
+			if ((this.getX() + this.getWidth() / 2 >= g.getX() - g.getWidth() / 2)
+					&& (this.getY() + this.getHeight() / 2 >= g.getY() - g.getHeight() / 2)
+					&& (this.getY() - this.getHeight() / 2 <= g.getY() + g.getHeight())) {
+				return true;
+			}
 
-                }
+			else {
+				return false;
+			}
 
- 
+		}
 
-                @Override
-                public void draw(PApplet p) {
+	}
 
-                                // TODO Auto-generated method stub
+	@Override
+	public void draw(PApplet p) {
 
-                               
+		// TODO Auto-generated method stub
 
-                }
-
- 
+	}
 
 }
