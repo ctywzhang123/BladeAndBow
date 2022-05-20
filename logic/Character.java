3e9204d5e839e0c4471dbf2c4db0545ff3b91ae4
FriedRice53 <97309425+FriedRice53@users.noreply.github.com>
2022-05-19 21:35:30 -0400

    Create Character.java

---
+++ logic/Character.java
@@ -0,0 +1,30 @@
+import processing.core.PApplet;
+
+public class Character extends GameObject {
+
+	public Character(int width, int height, int x, int y, int speed) {
+		super(width, height, x, y, speed);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	//Interface Methods:
+	@Override
+	public void move(int xMove, int yMove) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean collide(GameObject g) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void draw(PApplet p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
