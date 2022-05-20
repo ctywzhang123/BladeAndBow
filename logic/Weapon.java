d68fce985e7ee27cf1d6ff635a5c77e8ac5242db
Leonard <70354786+wuleo870@users.noreply.github.com>
2022-05-20 12:40:31 -0400

    Update Weapon.java

--- logic/Weapon.java
+++ logic/Weapon.java
@@ -1,29 +1,79 @@
 import processing.core.PApplet;
 
-public class Weapon extends GameObject {
+ 
 
-	public Weapon(int width, int height, int x, int y, int speed) {
-		super(width, height, x, y, speed);
-		// TODO Auto-generated constructor stub
-	}
+public class Weapon extends GameObject{
 
-	//Interface Methods:
-	@Override
-	public void move(int xMove, int yMove) {
-		// TODO Auto-generated method stub
-		
-	}
+ 
 
-	@Override
-	public boolean collide(GameObject g) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+       public Weapon(int width, int height, int x, int y, int speed) {
 
-	@Override
-	public void draw(PApplet p) {
-		// TODO Auto-generated method stub
-		
-	}
+              super(width, height, x, y, speed);
+
+       }
+
+ 
+
+       @Override
+
+       public void move(int xMove, int yMove) {
+
+              setX(getX() + xMove);
+
+              setY(getY() + yMove);           
+
+       }
+
+ 
+
+       @Override
+
+       public boolean collide(GameObject g) {
+
+              //Check if the Weapon hitbox collides with a player hitbox
+
+              //X and Y coordinates are the center of the image/hitbox rectangle
+
+              Character c = new Character(0, 0, 0, 0, 0);
+
+              if(g.getClass() != c.getClass()) //Ensure Weapon hits a player
+
+                     return false;
+
+              else {
+
+                     if((this.getX() + this.getWidth()/2 >= g.getX() - g.getWidth()/2) &&
+
+                                  (this.getY() + this.getHeight()/2 >= g.getY() - g.getHeight()/2) &&
+
+                                  (this.getY() - this.getHeight()/2 <= g.getY() + g.getHeight())) {
+
+                           return true;
+
+                     }
+
+                     else {
+
+                           return false;
+
+                     }
+
+              }
+
+       }
+
+ 
+
+       @Override
+
+       public void draw(PApplet p) {
+
+              // TODO Auto-generated method stub
+
+             
+
+       }
+
+ 
 
 }
