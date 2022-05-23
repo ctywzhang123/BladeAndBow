db9f4765bc5f6cb97b735f6c1fa39d7b163b34bb
FriedRice53 <97309425+FriedRice53@users.noreply.github.com>
2022-05-22 20:40:30 -0400

    Update PSprite.java

--- logic/PSprite.java
+++ logic/PSprite.java
@@ -1,30 +1,57 @@
+import java.io.File;
+
 import processing.core.PApplet;
 import processing.core.PImage;
 
-public abstract class PSprite extends GameObject{
-	private PImage img;
-	private int x, y, w, h;
-	
-	public PSprite(PImage img, int w, int h, int x, int y, int speed) {
-		super(w, h, x, y, speed);
-		this.img = img;
-		this.x = x;
-		this.y = y;
-		this.w = w;
-		this.h = h;
-	}
+public class PSprite extends GameObject{ //FIX!!!
+private File img;
+private int x, y, w, h;
 
-	public PImage getImg() {
-		return img;
-	}
+/**
+* Constructs a PSprite object
+* @param img - Image of the Sprite
+* @param w - width
+* @param h - height
+* @param x - x-coordinate
+* @param y - y-coordinate
+* @param speed - move speed
+*/
+public PSprite(File img, int w, int h, int x, int y) { //CHANGE PImage PARAMETER TO FILE PARAMETER!!!
+super(w, h, x, y, 1);
+this.img = img;
+this.x = x;
+this.y = y;
+this.w = w;
+this.h = h;
+}
 
-	public void setImg(PImage img) {
-		this.img = img;
-	}
-	
-	public void draw(PApplet p) {
-		p.image(img,  x,  y);
-	}
-	
+public File getImg() {
+return img;
+}
+
+public void setImg(File img) {
+this.img = img;
+}
+
+@Override
+public void draw(PApplet p) {
+PImage img = new PImage();
+p.loadImage(this.img.getName());
+img.resize(this.w, this.h);
+p.image(img,  x,  y);
+}
+
+@Override
+public void move(int xMove, int yMove) {
+// TODO Auto-generated method stub
+
+}
+
+@Override
+public boolean collide(GameObject g) {
+// TODO Auto-generated method stub
+return false;
+}
+
 
 }
