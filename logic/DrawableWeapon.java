a45d1eac2373a14f4f2af10dff210a9132fe4ca0
FriedRice53 <97309425+FriedRice53@users.noreply.github.com>
2022-05-23 19:10:21 -0400

    Create DrawableWeapon.java

---
+++ logic/DrawableWeapon.java
@@ -0,0 +1,24 @@
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class DrawableWeapon implements PDrawable {
+
+	private Weapon w;
+
+	public DrawableWeapon(Weapon w) {
+		this.w = w;
+	}
+
+	@Override
+	public void draw(PApplet p) {
+		PImage img = new PImage();
+		img = p.loadImage(w.getSprite().getFileName());
+		img.resize(w.getWidth(), w.getHeight());
+		p.image(img, w.getX(), w.getY());
+	}
+
+	public Weapon getWeapon() {
+		return w;
+	}
+
+}
