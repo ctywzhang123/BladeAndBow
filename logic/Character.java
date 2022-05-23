1009e3ca8ef44005484eb2b8f80ee586f93d764c
FriedRice53 <97309425+FriedRice53@users.noreply.github.com>
2022-05-22 23:12:48 -0400

    Update Character.java

--- logic/Character.java
+++ logic/Character.java
@@ -1,15 +1,15 @@
 import processing.core.PApplet;
-import processing.core.PImage;
 
 public class Character extends GameObject {
-	PImage sprite;
 
-	public Character(int width, int height, int x, int y, int speed) {
+	private PSprite ps;
+	
+	public Character(PSprite p, int width, int height, int x, int y, int speed) {
 		super(width, height, x, y, speed);
+		ps = p;
 	}
 
-	// Interface Methods:
-
+	//Interface Methods:
 	@Override
 	public void move(int xMove, int yMove) {
 		setX(getX() + xMove);
@@ -18,34 +18,24 @@ public class Character extends GameObject {
 
 	@Override
 	public boolean collide(GameObject g) {
-		// Check if the Weapon hitbox collides with a player hitbox
-
-		// X and Y coordinates are the center of the image/hitbox rectangle
-
-		Weapon w = new Weapon(0, 0, 0, 0, 0);
-
-		if (g.getClass() != w.getClass()) // Ensure Weapon hits a player
+		Weapon w = new Weapon(ps, 0, 0, 0, 0, 0);
+		if(g.getClass() != w.getClass()) {
 			return false;
+		}
 		else {
-			if ((this.getX() + this.getWidth() / 2 >= g.getX() - g.getWidth() / 2)
-					&& (this.getY() + this.getHeight() / 2 >= g.getY() - g.getHeight() / 2)
-					&& (this.getY() - this.getHeight() / 2 <= g.getY() + g.getHeight())) {
+			Weapon temp = (Weapon)g;
+			if(this.getHitbox().intersects(temp.getHitbox())) {
 				return true;
 			}
-
 			else {
 				return false;
 			}
-
 		}
-
 	}
 
 	@Override
 	public void draw(PApplet p) {
-
-		// TODO Auto-generated method stub
-
+		ps.draw(p);
 	}
 
 }
