915e2edb52e8cc5b73ee883d131d96aef69c5b69
Leonard <70354786+wuleo870@users.noreply.github.com>
2022-06-01 13:02:11 -0400

    Update DrawableCharacter.java

--- logic/DrawableCharacter.java
+++ logic/DrawableCharacter.java
@@ -4,23 +4,20 @@ import processing.core.PImage;
 public class DrawableCharacter implements PDrawable {
 
 	private Character c;
-	
-	public DrawableCharacter(Character c) {
+	PImage img = new PImage();
+
+	public DrawableCharacter(Character c, PApplet p) {
 		this.c = c;
-	}
-	
-	@Override
-	public void draw(PApplet p) {
-		PImage img = new PImage();
 		img = p.loadImage(c.getSprite().getFileName());
 		img.resize(c.getWidth(), c.getHeight());
+	}
+
+	@Override
+	public void draw(PApplet p) {
 		p.image(img, c.getX(), c.getY());
 	}
-	
+
 	public Character getCharacter() {
 		return c;
 	}
-	
 }
-
-
