a906648c8d1b80529a89c56f697dc7f754114b0d
Leonard <70354786+wuleo870@users.noreply.github.com>
2022-06-01 13:02:36 -0400

    Update DrawableWeapon.java

--- logic/DrawableWeapon.java
+++ logic/DrawableWeapon.java
@@ -4,15 +4,16 @@ import processing.core.PImage;
 public class DrawableWeapon implements PDrawable {
 
 	private Weapon w;
-	public DrawableWeapon(Weapon w) {
+	PImage img = new PImage();
+	
+	public DrawableWeapon(Weapon w, PApplet p) {
 		this.w = w;
+		img = p.loadImage(w.getSprite().getFileName());
+		img.resize(w.getWidth(), w.getHeight());
 	}
 	
 	@Override
 	public void draw(PApplet p) {
-		PImage img = new PImage();
-		img = p.loadImage(w.getSprite().getFileName());
-		img.resize(w.getWidth(), w.getHeight());
 		p.image(img, w.getX(), w.getY());
 	}
 	
@@ -21,5 +22,3 @@ public class DrawableWeapon implements PDrawable {
 	}
 
 }
-
-
