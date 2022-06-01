7495798a5c9545ec54034b7b5b7453dd189681d8
FriedRice53 <97309425+FriedRice53@users.noreply.github.com>
2022-05-31 23:15:51 -0400

    Update Character.java

--- logic/Character.java
+++ logic/Character.java
@@ -1,41 +1,107 @@
 import java.awt.Rectangle;
 
-public class Character extends GameObject {
+import processing.core.PApplet;
+
+public class Character extends GameObject implements Jumpable{
 	
-	PSprite temp = new PSprite(null, 0, 0, 0, 0);
+	private static int gravity = 1;
+	private int xVelocity;
+	private int yVelocity;
 	private boolean walk;
+	private boolean facingLeft; //TRUE = LEFT, FALSE = RIGHT
 	private boolean jump;
+	private boolean fall;
 	
-	public Character(PSprite p, int speed) {
+	//Player Health:
+	private static int health;
+	private int originalHealth;
+	
+	public Character(PSprite p, int speed, int playerHealth) {
 		super(p, p.getWidth(), p.getHeight(), p.getX(), p.getY(), speed);
+		xVelocity = 8;
+		yVelocity = 8;
+		health = playerHealth;
+		originalHealth = playerHealth;
 	}
 	
+	//Methods:
+	public boolean isFacingLeft() {
+		return facingLeft;
+	}
+	
+	public void setFacingLeft(boolean facingLeft) {
+		this.facingLeft = facingLeft;
+	}
+	
+	public int getXVelocity() {
+		return xVelocity;
+	}
+	
+	public int getYVelocity() {
+		return yVelocity;
+	}
+	
+	public void setXVelocity(int xVelocity) {
+		this.xVelocity = xVelocity;
+	}
+	
+	public void setYVelocity(int yVelocity) {
+		this.yVelocity = yVelocity;
+	}
+	
+	public int getOriginalHealth() {
+		return originalHealth;
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}	
+	
+	public boolean isJump() {
+		return jump;
+	}
+
+	public void setJump(boolean jump) {
+		this.jump = jump;
+	}
+	
+	public boolean isFall() {
+		return fall;
+	}
+	
+	public void setFall(boolean fall) {
+		this.fall = fall;
+	}
+		
 	//Interface Methods:
 	@Override
 	public void move(int xMove, int yMove) {
 		//UPDATE HITBOX
 		setX(getX() + xMove);
 		setY(getY() + yMove);
-		setHitbox(new Rectangle(getX(), getY(), getWidth(), getHeight()));
+		setHitbox(new Rectangle(this.getHitbox().x + xMove, this.getHitbox().y + yMove, getWidth(), getHeight()));
 	}
 
-	@Override //CHANGE BACK
+	@Override
 	public boolean collide(GameObject g) {
-		Weapon w = new Weapon(temp, 0); //TEMPORARY CHECK
-		if(g.getClass() != w.getClass()) {
-			return false;
+		Weapon temp = (Weapon)g;
+		if(this.getHitbox().intersects(temp.getHitbox())) {
+			return true;
 		}
 		else {
-			Weapon temp = (Weapon)g;
-			if(this.getHitbox().intersects(temp.getHitbox())) {
-				return true;
-			}
-			else {
-				return false;
-			}
+			return false;
 		}
 	}
 
+	@Override
+	public void jump(PApplet p) {
+		setXVelocity(getXVelocity() - gravity);
+		setYVelocity(getYVelocity() - gravity);
+	}
 }
 
 
