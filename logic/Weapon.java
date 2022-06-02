1cf4344770ec6c5e325aabea8c15d70f6aba1940
Leonard <70354786+wuleo870@users.noreply.github.com>
2022-06-02 12:49:36 -0400

    Update Weapon.java

--- logic/Weapon.java
+++ logic/Weapon.java
@@ -1,40 +1,95 @@
 import java.awt.Rectangle;
 
-public class Weapon extends GameObject {
-	
-	PSprite temp = new PSprite(null, 0, 0, 0, 0);
-	private boolean walk;
-	private boolean jump;
+ 
 
-	public Weapon(PSprite p, int speed) {
-		super(p, p.getWidth(), p.getHeight(), p.getX(), p.getY(), speed);
-	}
+public class Weapon extends GameObject implements Attackable {
 
-	//Interface Methods:
-	@Override
-	public void move(int xMove, int yMove) {
-		setX(getX() + xMove);
-		setY(getY() + yMove);
-		setHitbox(new Rectangle(getX(), getY(), getWidth(), getHeight()));
-	}
+      
 
-	@Override
-	public boolean collide(GameObject g) {
-		Character w = new Character(temp, 0);
-		if(g.getClass() != w.getClass()) {
-			return false;
-		}
-		else {
-			Character temp = (Character)g;
-			if(this.getHitbox().intersects(temp.getHitbox())) {
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-	}
+       PSprite temp = new PSprite(null, 0, 0, 0, 0);
+
+       private boolean walk;
+
+       private boolean jump;
+
+       private boolean attack = false;
+
+       private Rectangle hitbox;
+
+       private Rectangle originalHitbox;
+
+ 
+
+       public Weapon(PSprite p, int speed) {
+
+              super(p, p.getWidth(), p.getHeight(), p.getX(), p.getY(), speed);
+
+              this.hitbox = new Rectangle(p.getX(), p.getY(), p.getHeight(), p.getWidth());
+
+              this.originalHitbox = hitbox;
+
+       }
+
+ 
+
+       public void setAttack(boolean attack) {
+
+              this.attack = attack;
+
+       }
+
+      
+
+       //Interface Methods:
+
+       @Override
+
+       public void move(int xMove, int yMove) {
+
+              setX(getX() + xMove);
+
+              setY(getY() + yMove);
+
+              setHitbox(new Rectangle(getX(), getY(), getWidth(), getHeight())); //FIX
+
+       }
+
+ 
+
+       @Override
+
+       public boolean collide(GameObject g) {
+
+              if(!attack) {
+
+                     return false;
+
+              }
+
+              Character temp = (Character)g;
+
+              if(this.getHitbox().intersects(temp.getHitbox())) {
+
+                     return true;
+
+              }
+
+              else {
+
+                     return false;
+
+              }
+
+       }
+
+ 
+
+       @Override
+
+       public void attack() {
+
+              attack = true;
+
+       }
 
 }
-
-
