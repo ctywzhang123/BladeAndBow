13010095f40a8f5aa9c856748e6a3f8ac965d04a
FriedRice53 <97309425+FriedRice53@users.noreply.github.com>
2022-06-08 21:23:29 -0400

    Update Weapon.java

--- logic/Weapon.java
+++ logic/Weapon.java
@@ -1,107 +1,62 @@
 import java.awt.Rectangle;
 
 public class Weapon extends GameObject implements Attackable {
-
+	
 	PSprite temp = new PSprite(null, 0, 0, 0, 0);
-
-	private boolean walk;
-
-	private boolean jump;
-
 	private int damage = 4;
-
 	private double cooldown;
-
-	private int attack = -1; // -1 = READY (No attack), 0 = IN-PROGRESS/COOLDOWN, 1 = ATTACK REGISTERS
-
+	private int attack = -1; //-1 = READY (No attack), 0 = IN-PROGRESS/COOLDOWN, 1 = ATTACK REGISTERS
 	private Rectangle hitbox;
 
-	private Rectangle originalHitbox;
-
-	public Weapon(PSprite p, int speed, int damage, double cooldown) {
-
-		super(p, p.getWidth(), p.getHeight(), p.getX(), p.getY(), speed);
-
-		this.hitbox = new Rectangle(p.getX(), p.getY(), p.getHeight(), p.getWidth());
-
-		this.originalHitbox = hitbox;
-
+	public Weapon(PSprite pLeft, PSprite pRight, int speed, int damage, double cooldown) {
+		super(pLeft, pRight, pLeft.getWidth(), pLeft.getHeight(), pLeft.getX(), pLeft.getY(), speed);
+		this.hitbox = new Rectangle(pLeft.getX(), pLeft.getY(), pLeft.getHeight(), pLeft.getWidth());
 		this.damage = damage;
-
 		this.cooldown = cooldown;
-
 	}
-
+	
 	public int getAttack() {
-
 		return attack;
-
 	}
 
 	public void setAttack(int attack) {
-
 		this.attack = attack;
-
 	}
-
+	
 	public int getDamage() {
-
 		return damage;
-
 	}
-
+	
 	public double getCooldown() {
-
 		return cooldown;
-
 	}
-
-	// Interface Methods:
-
+	
+	//Interface Methods:
 	@Override
-
 	public void move(int xMove, int yMove) {
-
 		setX(getX() + xMove);
-
 		setY(getY() + yMove);
-
-		setHitbox(new Rectangle(getX() - getWidth() / 2, getY() - getHeight() / 2, getWidth(), getHeight())); // FIX
-
+		setHitbox(new Rectangle(getX() - getWidth()/2, getY() - getHeight()/2, getWidth(), getHeight())); //FIX
 	}
 
 	@Override
-
 	public boolean collide(GameObject g) {
-
-		if (attack == -1 || attack == 0) {
-
+		if(attack == -1 || attack == 0) {
 			return false;
-
 		}
-
-		Character temp = (Character) g;
-
-		if (this.getHitbox().intersects(temp.getHitbox())) {
-
+		Character temp = (Character)g;
+		if(this.getHitbox().intersects(temp.getHitbox())) {
 			return true;
-
 		}
-
 		else {
-
 			return false;
-
 		}
-
 	}
 
 	@Override
-
 	public void attack() {
-
 		attack = 1;
-
 	}
-
 }
+
+
