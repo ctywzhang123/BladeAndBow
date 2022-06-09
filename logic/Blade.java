24b7e17264cc3496d81d1184802d6ed73e0f36ec
FriedRice53 <97309425+FriedRice53@users.noreply.github.com>
2022-06-08 21:19:14 -0400

    Update Blade.java

--- logic/Blade.java
+++ logic/Blade.java
@@ -1,10 +1,11 @@
 
 public class Blade extends Weapon {
 
-	public Blade(PSprite p, int speed, int damage, int cooldown) {
-		super(p, speed, damage, cooldown);
+	public Blade(PSprite pLeft, PSprite pRight, int speed, int damage, double cooldown) {
+		super(pLeft, pRight, speed, damage, cooldown);
 	}
 
 }
+
 
 
